9af6c039999628bac01528a179722051f90028fe
易成杰 <626659321@qq.com>
2020-04-29 11:30:53 +0800

    保存

--- basic-middle/src/main/java/com/yicj/config/MvcConfig.java
+++ basic-middle/src/main/java/com/yicj/config/MvcConfig.java
@@ -2,8 +2,11 @@ package com.yicj.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -19,6 +22,16 @@ public class MvcConfig extends WebMvcConfigurationSupport {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager() ;
+        manager.createUser(User.withUsername("user").password("123").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("123").roles("USER","ADMIN").build());
+        return manager ;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
--- basic-middle/src/main/java/com/yicj/config/WebSecurityConfig.java
+++ basic-middle/src/main/java/com/yicj/config/WebSecurityConfig.java
@@ -20,42 +20,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         http.authorizeRequests()
-            .antMatchers("/","/home").permitAll()
+            .antMatchers("/admin/api/**").hasRole("ADMIN")
+            .antMatchers("/user/api/**").hasRole("USER")
+            .antMatchers("/app/api/**").permitAll()
             .anyRequest().authenticated()
             .and()
         .formLogin()
-            .loginPage("/login")
-            .loginProcessingUrl("/login")
-            //指定登录成功时的处理逻辑
-            .successHandler((request, response, authentication)->{
-                response.setContentType("application/json;charset=UTF-8");
-                PrintWriter out = response.getWriter();
-                //{"error_code":"0","message":"欢迎登录系统"}
-                out.write("{\"error_code\":\"0\",\"message\":\"欢迎登录系统\"}");
-            })
-            //指定登录失败时的处理逻辑
-            .failureHandler((request, response, ex) ->{
-                response.setContentType("application/json;charset=UTF-8");
-                response.setStatus(401);
-                PrintWriter out = response.getWriter();
-                //输出失败原因
-                //{"error_code":"401","name":"","message":""}
-                out.write("{\"error_code\":\"401\",\"name\":\""+ ex.getClass()+"\",\"message\":\""+ ex.getMessage()+"\"}");
-            })
-            .permitAll()
-            .and()
-        .httpBasic()
-            .and()
-        .logout()
             .permitAll()
             .and()
         .csrf().disable();
     }
 
-    @Override
+    //这里可以配置用户名密码信息
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //super.configure(auth);
         auth.inMemoryAuthentication()
             .withUser("user").password("123").roles("USER") ;
-    }
+    }*/
 }
