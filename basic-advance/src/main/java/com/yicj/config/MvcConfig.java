10ba314bbcae83e0ae42690083c926a32497ade2
易成杰 <626659321@qq.com>
2020-04-29 21:25:54 +0800

    自定义provider验证图形验证码

--- basic-advance/src/main/java/com/yicj/config/MvcConfig.java
+++ basic-advance/src/main/java/com/yicj/config/MvcConfig.java
@@ -1,5 +1,8 @@
 package com.yicj.config;
 
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 
 @Configuration
@@ -30,15 +34,6 @@ public class MvcConfig extends WebMvcConfigurationSupport {
     }
 
 
-   /* @Bean
-    public UserDetailsService userDetailsService(){
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager() ;
-        manager.createUser(User.withUsername("user").password("123").roles("USER").build());
-        manager.createUser(User.withUsername("admin").password("123").roles("USER","ADMIN").build());
-        return manager ;
-    }*/
-
-
     @Bean
     public UserDetailsService userDetailsService(){
         JdbcUserDetailsManager manager = new JdbcUserDetailsManager() ;
@@ -50,6 +45,25 @@ public class MvcConfig extends WebMvcConfigurationSupport {
             manager.createUser(User.withUsername("admin").password("123").roles("USER","ADMIN").build());
         }
         return manager ;
+    }
+
+    @Bean
+    public Producer captcha(){
+        //配置图形验证码的基本参数
+        Properties properties = new Properties() ;
+        //图片宽度
+        properties.setProperty("kaptcha.image.width","150") ;
+        //图片长度
+        properties.setProperty("kaptcha.image.height","50") ;
+        //字符集
+        properties.setProperty("kaptcha.textproducer.char.string","0123456789") ;
+        //字符长度
+        properties.setProperty("kaptcha.textproducer.char.length","4") ;
+        Config config = new Config(properties);
+        //使用默认的图片验证码实现，当然也可以自定义实现
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha() ;
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
     }
 
 
--- basic-advance/src/main/java/com/yicj/config/WebSecurityConfig.java
+++ basic-advance/src/main/java/com/yicj/config/WebSecurityConfig.java
@@ -1,20 +1,24 @@
 package com.yicj.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails>  authenticationDetailsSource ;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider ;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,15 +26,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
             .antMatchers("/admin/api/**").hasRole("ADMIN")
             .antMatchers("/user/api/**").hasRole("USER")
-            .antMatchers("/app/api/**").permitAll()
+            .antMatchers("/app/api/**","/captcha.jpg").permitAll()
             .anyRequest().authenticated()
             .and()
         .csrf().disable()
         .formLogin()
+            //应用AutheticationDetailsSource
+            .authenticationDetailsSource(authenticationDetailsSource)
+            .loginPage("/login")
+            .loginProcessingUrl("/auth/form")
             .permitAll()
-            .and()
-        .sessionManagement()
-            .maximumSessions(1) ;
+            .failureHandler(new MyAuthenticationFailureHandler()) ;
 
     }
 
