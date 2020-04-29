526b9338a9d5ee5513eae5a9532d1c72f479b7f8
易成杰 <626659321@qq.com>
2020-04-29 10:59:49 +0800

    基本介绍为基础做升级

---
+++ basic-middle/src/main/java/com/yicj/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.yicj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance() ;
+    }
+}
