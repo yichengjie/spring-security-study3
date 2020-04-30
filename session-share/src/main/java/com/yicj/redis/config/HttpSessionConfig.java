804df9d3dccdb354ff896163ad0444136738d37b
易成杰 <626659321@qq.com>
2020-04-30 17:40:58 +0800

    保存

--- session-share/src/main/java/com/yicj/redis/config/HttpSessionConfig.java
+++ session-share/src/main/java/com/yicj/redis/config/HttpSessionConfig.java
@@ -1,39 +1,15 @@
 package com.yicj.redis.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
-import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 //启动基于Redis的HttpSession实现
 @EnableRedisHttpSession
 public class HttpSessionConfig {
 
-    @Autowired
-    private FindByIndexNameSessionRepository sessionRepository ;
 
-    //提供Redis连接，默认localhost:6379
-    @Bean
-    public RedisConnectionFactory connectionFactory(){
-        return new JedisConnectionFactory() ;
-    }
-
-    //SpringSessionBackedSessionRegistry是session为Spring Security提供的
-    //用于在集群环境下控制会话并发的会话注册实现类
-    @Bean
-    public SpringSessionBackedSessionRegistry sessionRegistry(){
-
-        return new SpringSessionBackedSessionRegistry(sessionRepository) ;
-    }
-
-    //HttpSession 的事件监听，改为session提供的会话注册表
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher(){
-        return new HttpSessionEventPublisher() ;
-    }
 
 }
--- session-share/src/main/java/com/yicj/redis/config/WebSecurityConfig.java
+++ session-share/src/main/java/com/yicj/redis/config/WebSecurityConfig.java
@@ -1,14 +1,48 @@
 package com.yicj.redis.config;
 
 import com.yicj.redis.vercode.MyAuthenticationFailureHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SpringSessionBackedSessionRegistry redisSessionRegistry ;
+
+    @Autowired
+    private FindByIndexNameSessionRepository sessionRepository ;
+
+    //提供Redis连接，默认localhost:6379
+    @Bean
+    public RedisConnectionFactory connectionFactory(){
+        return new JedisConnectionFactory() ;
+    }
+
+
+    //HttpSession 的事件监听，改为session提供的会话注册表
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher(){
+        return new HttpSessionEventPublisher() ;
+    }
+
+    //SpringSessionBackedSessionRegistry是session为Spring Security提供的
+    //用于在集群环境下控制会话并发的会话注册实现类
+    @Bean
+    public SpringSessionBackedSessionRegistry sessionRegistry(){
+
+        return new SpringSessionBackedSessionRegistry(sessionRepository) ;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,8 +56,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .csrf().disable()
         .formLogin()
             .permitAll()
-            .failureHandler(new MyAuthenticationFailureHandler()) ;
-
+            .failureHandler(new MyAuthenticationFailureHandler())
+            .and()
+        .sessionManagement()
+            .maximumSessions(1)
+            //使用session提供的会话注册表
+            .sessionRegistry(redisSessionRegistry);
     }
 
 }
