f831a4a46a684cc5ce032f8769938c3d4f625d72
易成杰 <626659321@qq.com>
2020-04-30 20:31:48 +0800

    保存

--- session-share-security/src/main/java/com/yicj/redis/config/MvcConfig.java
+++ session-share-security/src/main/java/com/yicj/redis/config/MvcConfig.java
@@ -1,8 +1,15 @@
 package com.yicj.redis.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -27,6 +34,7 @@ public class MvcConfig extends WebMvcConfigurationSupport {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
+
 
     @Bean
     public UserDetailsService userDetailsService(){
--- session-share/src/main/java/com/yicj/redis/config/WebSecurityConfig.java
+++ session-share/src/main/java/com/yicj/redis/config/WebSecurityConfig.java
@@ -1,67 +1,36 @@
 package com.yicj.redis.config;
 
-import com.yicj.redis.vercode.MyAuthenticationFailureHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
-import org.springframework.session.FindByIndexNameSessionRepository;
-import org.springframework.session.security.SpringSessionBackedSessionRegistry;
+
+import java.io.PrintWriter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private SpringSessionBackedSessionRegistry redisSessionRegistry ;
-
-    @Autowired
-    private FindByIndexNameSessionRepository sessionRepository ;
-
-    //提供Redis连接，默认localhost:6379
-    @Bean
-    public RedisConnectionFactory connectionFactory(){
-        return new JedisConnectionFactory() ;
-    }
-
-
-    //HttpSession 的事件监听，改为session提供的会话注册表
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher(){
-        return new HttpSessionEventPublisher() ;
-    }
-
-    //SpringSessionBackedSessionRegistry是session为Spring Security提供的
-    //用于在集群环境下控制会话并发的会话注册实现类
-    @Bean
-    public SpringSessionBackedSessionRegistry sessionRegistry(){
-
-        return new SpringSessionBackedSessionRegistry(sessionRepository) ;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         http.authorizeRequests()
-            .antMatchers("/admin/api/**").hasRole("ADMIN")
-            .antMatchers("/user/api/**").hasRole("USER")
-            .antMatchers("/app/api/**","/favicon.ico").permitAll()
-            .anyRequest().authenticated()
-            .and()
-        .csrf().disable()
-        .formLogin()
-            .permitAll()
-            .failureHandler(new MyAuthenticationFailureHandler())
-            .and()
-        .sessionManagement()
-            .maximumSessions(1)
-            //使用session提供的会话注册表
-            .sessionRegistry(redisSessionRegistry);
+                .antMatchers("/admin/api/**").hasRole("ADMIN")
+                .antMatchers("/user/api/**").hasRole("USER")
+                .antMatchers("/app/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .permitAll()
+                .and()
+            .csrf().disable();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication()
+            .withUser("user").password("123").roles("USER") ;
+    }
 }
