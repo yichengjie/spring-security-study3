1eed869d15d2ffadc6b1d5544d43b1608c10325d
易成杰 <626659321@qq.com>
2020-04-30 13:43:30 +0800

    删除验证码相关代码

--- session-share/src/main/java/com/yicj/SessionShareApplication.java
+++ session-share/src/main/java/com/yicj/SessionShareApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyApplication {
+public class SessionShareApplication {
     public static void main(String[] args)  {
-        //dXNlciUzQTEyMw==
         //Basic dXNlcjoxMjM=
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(SessionShareApplication.class, args);
     }
 }
--- session-share/src/main/java/com/yicj/config/WebSecurityConfig.java
+++ session-share/src/main/java/com/yicj/config/WebSecurityConfig.java
@@ -1,26 +1,14 @@
 package com.yicj.config;
 
 import com.yicj.vercode.MyAuthenticationFailureHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails>  myAuthenticationDetailsSource ;
-
-    @Autowired
-    private AuthenticationProvider authenticationProvider ;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,8 +23,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .httpBasic()
             .and()
         .formLogin()
-            //应用AutheticationDetailsSource
-            .authenticationDetailsSource(myAuthenticationDetailsSource)
             .loginPage("/login")
             .loginProcessingUrl("/auth/form")
             .permitAll()
@@ -44,8 +30,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider) ;
-    }
 }
