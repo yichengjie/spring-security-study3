aeda218466f10b255e6943c9f342f30df95a84d7
易成杰 <626659321@qq.com>
2020-05-02 13:05:09 +0800

    保存

---
+++ security-demo2/src/main/java/com/yicj/SecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yicj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoApplication {
+    public static void main(String[] args)  {
+        //Basic dXNlcjoxMjM=
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+}
--- security-demo2/src/main/java/com/yicj/config/BrowserSecurityConfig.java
+++ security-demo2/src/main/java/com/yicj/config/BrowserSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.yicj.browser.config;
+package com.yicj.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
