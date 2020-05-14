0b6ee2b8c7ac21a77fc00e4065e2f2ffbf43971c
易成杰 <626659321@qq.com>
2020-05-14 19:53:59 +0800

    保存

---
+++ oauth-server-study/client-for-simple-server/src/main/java/com/yicj/study/ClientForSimpleServerApplication.java
@@ -0,0 +1,15 @@
+package com.yicj.study;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientForSimpleServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClientForSimpleServerApplication.class, args) ;
+    }
+
+}
