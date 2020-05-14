8c08d2ff35c0b7b6a1f8c72b1a86c239c37a88f6
易成杰 <626659321@qq.com>
2020-05-14 09:20:55 +0800

    替换为springboot2

--- oauth-server-study/oauth-server-simple/src/main/java/com/yicj/study/config/ResourceServerConfig.java
+++ oauth-server-study/oauth-server-simple/src/main/java/com/yicj/study/config/ResourceServerConfig.java
@@ -4,6 +4,7 @@ package com.yicj.study.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -16,10 +17,10 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
  * 当请求中包含OAuth2 access_token时，Spring Security会根据WebSecurityConfigurationAdapter，
  * 执行顺序(Order)是3.在SecurityConfiguration类之前执行，优先级更高
  */
+@Slf4j
 @Profile("simple")
 @Configuration
 @EnableResourceServer
-@Slf4j
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public static final String RESOURCE_ID = "authorizationserver" ;
 
--- oauth-server-study/oauth-server-simple/src/main/java/com/yicj/study/config/SecurityConfiguration.java
+++ oauth-server-study/oauth-server-simple/src/main/java/com/yicj/study/config/SecurityConfiguration.java
@@ -4,12 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
