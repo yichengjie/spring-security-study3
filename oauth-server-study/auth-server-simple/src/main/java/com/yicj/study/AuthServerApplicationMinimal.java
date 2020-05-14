5c4a684774ee843eb417cbc6f5d8ff49492feb29
易成杰 <626659321@qq.com>
2020-05-14 09:59:22 +0800

    保存

--- oauth-server-study/auth-server-simple/src/main/java/com/yicj/study/AuthServerApplicationMinimal.java
+++ oauth-server-study/auth-server-simple/src/main/java/com/yicj/study/AuthServerApplicationMinimal.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
-//@Profile("minimal")
-//@SpringBootApplication
-//@EnableAuthorizationServer
-//public class AuthServerApplicationMinimal {
-//
-//    public static void main(String[] args) {
-//
-//        SpringApplication.run(AuthServerApplicationMinimal.class, args);
-//    }
-//
-//}
+@Profile("minimal")
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthServerApplicationMinimal {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AuthServerApplicationMinimal.class, args);
+    }
+
+}
