ea9623b8a56f725452ef1d91f6cf9db0def0b88c
易成杰 <626659321@qq.com>
2020-05-17 12:26:31 +0800

    保存

--- imooc-security-study/security-app/src/main/java/com/yicj/app/config/ImoocResourceServerConfig.java
+++ imooc-security-study/security-app/src/main/java/com/yicj/app/config/ImoocResourceServerConfig.java
@@ -1,9 +1,25 @@
 package com.yicj.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
-public class ImoocResourceServerConfig {
+public class ImoocResourceServerConfig  extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+            .requestMatchers()
+                .antMatchers("/api/**")
+                .antMatchers("/user/api/**")
+        ;
+
+    }
+
 }
